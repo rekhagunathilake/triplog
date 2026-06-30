cfb11a9e5e6716982331d126577a143d028aea89
raygunathilake <patience0112@gmail.com>
2026-06-30 20:17:41 +0530

    Add scaffold for the application layer

---
+++ backend/Triplog.Entries.Application/Abstractions/ITripRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triplog.Entries.Application.Abstractions
+{
+    internal interface ITripRepository
+    {
+    }
+}
