07ce332797f6d88336974908b9041f0d30f10532
raygunathilake <patience0112@gmail.com>
2026-07-08 21:06:03 +0530

    Add unitofwork implementation

---
+++ backend/Triplog.Entries.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Triplog.Entries.Application.Abstractions;
+
+namespace Triplog.Entries.Infrastructure.Persistence
+{
+    public sealed class UnitOfWork(TriplogEntriesDbContext dbContext) : IUnitOfWork
+    {
+        public async Task<int> SaveChangesAsync(CancellationToken ct = default) =>
+            await dbContext.SaveChangesAsync(ct);
+    }
+}
