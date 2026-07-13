b8d9afc95157484fb6cd787f18b1bb758d2c391b
raygunathilake <patience0112@gmail.com>
2026-07-13 20:10:42 +0530

    Add self migration

--- backend/Triplog.Entries.Api/Program.cs
+++ backend/Triplog.Entries.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Triplog.Entries.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
@@ -10,6 +13,13 @@ builder.Services.AddApplication();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<TriplogEntriesDbContext>();
+    await db.Database.MigrateAsync();
+}
 
 app.MapDefaultEndpoints();
 
