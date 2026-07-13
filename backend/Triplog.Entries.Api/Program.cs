8ddd4be4d9f9c4bbd0607a1e257c8730bbd9bccf
raygunathilake <patience0112@gmail.com>
2026-07-13 23:28:06 +0530

    Add exceptionhandler

--- backend/Triplog.Entries.Api/Program.cs
+++ backend/Triplog.Entries.Api/Program.cs
@@ -30,6 +30,8 @@ if (app.Environment.IsDevelopment())
     await db.Database.MigrateAsync();
 }
 
+app.UseExceptionHandler();
+
 app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
