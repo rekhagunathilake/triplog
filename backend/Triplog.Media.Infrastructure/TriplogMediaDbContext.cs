0e5e03292d2832c93b153dc864fcbc273339f5af
raygunathilake <patience0112@gmail.com>
2026-07-22 17:24:31 +0530

    Add Media DB context

---
+++ backend/Triplog.Media.Infrastructure/TriplogMediaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Triplog.Media.Domain.MediaItems;
+
+namespace Triplog.Media.Infrastructure;
+
+public sealed class TriplogMediaDbContext(DbContextOptions<TriplogMediaDbContext> options)
+: DbContext(options)
+{
+    public DbSet<MediaItem> MediaItems => Set<MediaItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TriplogMediaDbContext).Assembly);
+    }
+}
---
+++ backend/Triplog.Media.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Triplog.Media.Application.Abstractions;
+
+namespace Triplog.Media.Infrastructure;
+
+public sealed class UnitOfWork(TriplogMediaDbContext dbContext) : IUnitOfWork
+{
+    public async Task<int> SaveChangesAsync(CancellationToken ct = default) =>
+        await dbContext.SaveChangesAsync(ct);
+}
